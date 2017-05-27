18baeb0a71748e64f9d05fb467ccdbbc2036cf38
Fahim Md Rafiq <fahim6331006@gmail.com>
2017-05-27 19:10:10 +0600

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void pressButton(ActionEvent event){
+        System.out.println("Hello madafaka");
+    }
+    public void pressButton02(ActionEvent event){
+        System.out.println("Hello madafaka");
+    }
+    public void pressButton03(ActionEvent event){
+        System.out.println("Hello madafaka");
+    }
+    public void pressButton04(ActionEvent event){
+        System.out.println("Hello madafaka4");
+    }
+}
