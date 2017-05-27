de7ff5f810433c1185deb21f09aa015579db0036
Fahim Md Rafiq <fahim6331006@gmail.com>
2017-05-28 00:13:28 +0600

    still goofing around

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,16 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 
 public class Controller {
-    public void pressButton(ActionEvent event){
-        System.out.println("Hello madafaka");
-    }
-    public void pressButton02(ActionEvent event){
-        System.out.println("Hello madafaka");
-    }
-    public void pressButton03(ActionEvent event){
-        System.out.println("Hello madafaka");
-    }
-    public void pressButton04(ActionEvent event){
-        System.out.println("Hello madafaka4");
+    void print(ActionEvent event) {
+
     }
 }
